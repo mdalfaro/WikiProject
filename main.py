146cf796a9965210077ad6bfdafa72f8bbc7177f
Max Alfaro <maxalfaro@maxs-mbp.attlocal.net>
2020-03-14 21:10:55 -0700

    adding R forecast program

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import download_history
+import subprocess
 
 def main():
+	# Download Chrome history
 	download_history.downloadHistory()
+
+	# Predict into future
+	bashCommand = "Rscript wiki_ts.R"
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output, error = process.communicate()
+
 
 	#start_new_thread(start_server.startServer(),())
 	#start_server.openBrowser()
